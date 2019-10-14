1c12cd338588d2657ce33b4033660e8316ca84f2
Dan <dansonlinepresence@gmail.com>
2019-10-14 16:16:20 -0400

    enabled/disable

--- client.js
+++ client.js
@@ -1,7 +1,14 @@
-const params = new URLSearchParams(window.location.search);
-const hostname = params.get('wslog_hostname') || window.location.hostname;
-const socket = new WebSocket(`ws://${hostname}:9111`);
+const enabled = 1;
 
-export function wslog() {
-  socket.send(Array.from(arguments).map(i => JSON.stringify(i)).join(' '));
+var wslog = () => {};
+
+if (enabled) {
+  const params = new URLSearchParams(window.location.search);
+  const hostname = params.get('wslog_hostname') || window.location.hostname;
+  const socket = new WebSocket(`ws://${hostname}:9111`);
+  wslog = function() {
+    socket.send(Array.from(arguments).map(i => JSON.stringify(i)).join(' '));
+  }
 }
+
+export { wslog };
